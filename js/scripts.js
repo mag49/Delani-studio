187eebc03aa788f71dbb3ddaa06b84f49a98b827
Ngethe <ciru930@gmail.com>
2019-12-10 04:21:07 +0300

    add styles to portfolio section

---
+++ js/scripts.js
@@ -0,0 +1,35 @@
+
+
+
+
+
+
+    $(document).ready(function(){
+        $(".info1").hide();
+        $('.design').click(function(){
+            $('.info1').toggle();
+            $('#img1').hide();
+    
+        });
+        $
+    });
+    
+    $(document).ready(function(){
+        $('.info2').hide();
+        $('.develop').click(function(){
+            $('.info2').toggle();
+            $('#img2').toggle();
+    
+        });
+        $
+    });
+    
+    $(document).ready(function(){
+        $('.info3').hide();
+        $('.product').click(function(){
+            $('.info3').toggle();
+            $('#img3').toggle();
+    
+        });
+        $
+    });
